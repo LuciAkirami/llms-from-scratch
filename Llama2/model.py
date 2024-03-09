1d77b46a426e624e673c6b3b0df17fcb26b29715
LuciAkirami <ajsubscriber001@gmail.com>
2024-03-09 18:27:23 +0530

    Added Model Args

--- Llama2/model.py
+++ Llama2/model.py
@@ -5,3 +5,19 @@ import math
 from dataclasses import dataclass
 from typing import Optional
 
+@dataclass
+class ModelArgs:
+    dim: int = 4096
+    n_layers: int = 32
+    n_heads: int = 32 # Num heads for queries
+    n_kv_heads: Optional[int] = None # Num heads for k and v 
+    vocab_size: int = -1 # will be set when we load the tokenizer
+    multiple_of: int = 256
+    ffn_dim_multiplier: Optional[float] = None
+    norm_eps: float = 1e-5
+
+    # Needed to KV cache
+    max_batch_size: int = 32
+    max_seq_len: int = 2048
+
+    device: str = None

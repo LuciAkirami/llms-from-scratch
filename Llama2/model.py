aff965e7b6dfbb65b751acd7b912e0542b90e251
LuciAkirami <ajsubscriber001@gmail.com>
2024-03-10 14:56:13 +0530

    Added Rotary Positional Embeddings

--- Llama2/model.py
+++ Llama2/model.py
@@ -9,7 +9,9 @@ from typing import Optional
 @dataclass
 class ModelArgs:
     dim: int = 4096
-    n_layers: int = 32 # Num times to repeat the model i.e from RMSNorm to Feed Forward SwiGLU
+    n_layers: int = (
+        32  # Num times to repeat the model i.e from RMSNorm to Feed Forward SwiGLU
+    )
     n_heads: int = 32  # Num heads for queries
     n_kv_heads: Optional[int] = None  # Num heads for k and v
     vocab_size: int = -1  # will be set when we load the tokenizer
@@ -22,6 +24,57 @@ class ModelArgs:
     max_seq_len: int = 2048
 
     device: str = None
+
+
+def pre_compute_theta_pos_frequencies(
+    head_dim: int, seq_len: int, device: str, theta: 10000.0
+):
+    # According to Llama paper, the embedding dimension must be even
+    assert head_dim % 2 == 0, "Head Dimension is not even"
+    # Building the theta parameters
+    # According to foruma that_i = 10000 ^ (-2(i-1)/dim) for i = [1,2,3..dim/2]
+    # Shape: (Head_Dim/2)
+    theta_numerator = torch.arange(0, head_dim, 2).float()
+    # Shape: (Head_Dim/2)
+    theta = theta ** -(theta_numerator / head_dim).to(device)
+
+    # Construct Positions("m" parameter)
+    # Shape: (Seq_Len)
+    m = torch.arange(seq_len).to(device)
+
+    # Multiply each theta by each position using user product
+    # Shape: (Seq_Len) outerproduct* (Head_Dim/2) -> (Seq_Len, Head_Dim/2)
+    freqs = torch.outer(m, theta).float()
+
+    # We can compute complex numbers in polar form c = R * exp(i * m * theta), where R=1
+    # so each element in the freq_complex will be like (a + ib) as complex numbers
+    # Shape: (Seq_Len, Head_Dim/2) -> (Seq_Len, Head_Dim/2)
+    freq_complex = torch.polar(torch.ones_like(freqs), freqs)
+    return freq_complex
+
+def apply_rotatry_embeddings(x: torch.Tensor, freq_complex: torch.Tensor, device: str):
+    # H - Num Heads, B - Batch Size
+    # We perform reshape, as the view_as_complex needs last dimension as 2
+    # Shape: (B, Seq_Len, H, Head_Dim) -> (B, Seq_Len, H, Head_Dim/2)
+    x_complex = torch.view_as_complex(x.float().reshape(*x.shape[:-1], -1, 2))
+
+    # Reshaping freq_complex to match x_complex
+    # Shape: (Seq_Len, Head_Dim/2) -> (1, Seq_Len, 1, Head_Dim/2)
+    freq_complex = freq_complex.unsqueeze(0).unsqueeze(2)
+
+    # Multiplying x_complex with freq_complex i.e performing rotation
+    # Shape: (B, Seq_Len, H, Head_Dim/2) * (1, Seq_Len, 1, Head_Dim/2) -> (B, Seq_Len, H, Head_Dim/2)
+    x_rotated = x_complex * freq_complex
+
+    # Viewing as Real instead Complex
+    # Shape: (B, Seq_Len, H, Head_Dim/2) -> (B, Seq_Len, H, Head_Dim/2, 2)
+    x_out = torch.view_as_real(x_rotated)
+
+    # reshaping
+    # Shape: (B, Seq_Len, H, Head_DimHead_Dim/2, 2) - > (B, Seq_Len, H, Head_DimHead_Dim)
+    x_out = x_rotated.reshape(*x.shape)
+
+    return x_out.type_as(x).to(device)
 
 
 class Transformer:
@@ -37,7 +90,7 @@ class Transformer:
         self.layers = nn.ModuleList()
 
         for _ in range(self.n_layers):
-            self.layers.append(Encoder(args))
+            self.layers.append(Decoder(args))
 
         self.norm = RMSNorm(args.dim, eps=args.eps)
         self.output = nn.Linear(args.dim, self.vocab_size, bias=True)

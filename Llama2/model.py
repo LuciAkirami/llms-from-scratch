f424e5b9df160ed5a223dcecaf1e24bb646386f1
LuciAkirami <ajsubscriber001@gmail.com>
2024-03-10 18:12:57 +0530

    Added Grouped Multi Query Attentiuon

--- Llama2/model.py
+++ Llama2/model.py
@@ -52,6 +52,7 @@ def pre_compute_theta_pos_frequencies(
     freq_complex = torch.polar(torch.ones_like(freqs), freqs)
     return freq_complex
 
+
 def apply_rotatry_embeddings(x: torch.Tensor, freq_complex: torch.Tensor, device: str):
     # H - Num Heads, B - Batch Size
     # We perform reshape, as the view_as_complex needs last dimension as 2
@@ -76,6 +77,7 @@ def apply_rotatry_embeddings(x: torch.Tensor, freq_complex: torch.Tensor, device
 
     return x_out.type_as(x).to(device)
 
+
 class RMSNorm(nn.Module):
 
     def __init__(self, dim: int, eps: float = 1e-6):
@@ -84,17 +86,113 @@ class RMSNorm(nn.Module):
         self.eps = eps
         # gamma parameter
         self.weight = nn.Parameter(torch.ones(dim))
-    
+
     def _norm(self, x: torch.Tensor):
-        # Mean is taken for squared elements for each sequence 
+        # Mean is taken for squared elements for each sequence
         # rsqrt: 1/sqrt(x)
         # (B, Seq_Len, Dim) * (B, Seq_Len * 1) = (B, Seq_Len, Dim)
-        norm = x * torch.rsqrt(x.pow(2).mean(-1,keepdim=True) + self.eps)
+        norm = x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
         return norm
-    
+
     def forward(self, x: torch.Tensor):
         # (Dim) * (B, Seq_Len, Dim) = (B, Seq_Len, Dim)
         return self.weight * self._norm(x.float()).type_as(x)
+
+
+def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
+    batch_size, seq_len, n_kv_heads, head_dim = x.shape
+
+    if n_rep == 1:
+        return x
+    else:
+        return (
+            # (B, Seq_Len, H_KV, 1, Head_Dim)
+            x[:, :, :, None, :]  # creating a new dimension 1, before Head_Dim
+            .expand(
+                batch_size, seq_len, n_kv_heads, n_rep, head_dim
+            )  # repeating the 1 Dim n_rep times
+            .reshape(batch_size, seq_len, n_kv_heads, n_rep * head_dim)
+            # reshaping to (B, Seq_Len, H_Q, Head_Dim) as H_KV * n_rep = H_Q
+        )
+
+# Performing a Grouped Multi Query Attention
+class SelfAttention(nn.Module):
+
+    def __init__(self, args):
+        super().__init__()
+
+        self.n_heads_q = args.n_heads
+        self.dim = args.dim
+        self.n_kv_heads = args.n_heads if args.n_kv_heads is None else args.n_kv_heads
+        self.n_rep = self.n_heads_q // self.n_kv_heads
+        self.head_dim = self.dim // self.n_heads_q
+
+        self.wq = nn.Linear(args.dim, args.n_heads * self.head_dim, bias=False)
+        self.wk = nn.Linear(args.dim, self.n_kv_heads * self.head_dim, bias=False)
+        self.wv = nn.Linear(args.dim, self.n_kv_heads * self.head_dim, bias=False)
+        self.wo = nn.Linear(args.n_heads * self.head_dim, args.dim, bias=False)
+
+        self.cache_k = torch.zeros(
+            (args.max_batch_size, args.max_seq_length, self.n_kv_heads, self.head_dim)
+        )
+        self.cache_v = torch.zeros(
+            (args.max_batch_size, args.max_seq_length, self.n_kv_heads, self.head_dim)
+        )
+
+    def forward(self, x: torch.Tensor, start_pos: str, freq_complex: torch.Tensor):
+        batch_size, seq_len, _ = x.shape  # (B, 1, Dim)
+
+        # Apply the Wq, Wk, Wv matrices to queries, keyes and values
+        # (B, 1, Dim) -> (B, 1, H_Q * Head_Dim)
+        xq = self.wq(x)
+        # (B, 1, Dim) -> (B, 1, H_KV * Head_Dim)
+        xk, xv = self.wk(x), self.wv(x)
+
+        # (B, 1, H_Q * Head_Dim) -> (B, 1, H_Q , Head_Dim)
+        xq = xq.view(batch_size, seq_len, self.n_heads_q, self.head_dim)
+
+        # (B, 1, H_Q * Head_Dim) -> (B, 1, H_KV , Head_Dim)
+        # The size of xk and xv can be less than the size of xq as H_Q * Head_Dim >= H_KV * Head_Dim
+        xk = xk.view(batch_size, seq_len, self.n_kv_heads, self.head_dim)
+        xv = xv.view(batch_size, seq_len, self.n_kv_heads, self.head_dim)
+
+        # Does not change the shape of the tensor
+        xq = apply_rotatry_embeddings(xq, freq_complex)
+        xk = apply_rotatry_embeddings(xk, freq_complex)
+
+        # Replace the entry in the cache for this token
+        self.cache_k[:batch_size, start_pos : start_pos + seq_len] = xq
+        self.cache_v[:batch_size, start_pos : start_pos + seq_len] = xv
+
+        # Retrieve all the cached keys and values so far
+        # (B, Seq_len_KV, H_KV, Head_Dim)
+        # the Seq_len_KV is equal to start_pos + 1, that is retrieve upto the start position
+        keys = self.cache_k[:batch_size, 0 : start_pos + seq_len]
+        values = self.cache_v[:batch_size, 0 : start_pos + seq_len]
+
+        # Num of heads of KV may not match num of heads of Q
+        # Hence repeat KV heads n_rep times to match the heads of Q
+        keys = repeat_kv(keys, self.n_rep)
+        values = repeat_kv(values, self.n_rep)
+
+        # Transposing before calculating attention
+        # (B, 1, H_Q, Head_Dim) -> (B, H_Q, 1, Head_Dim)
+        xq = xq.transpose(1,2)
+        keys = keys.transpose(1,2)
+        values = values.transpose(1,2)
+
+        # (B, H_Q, 1, Head_Dim) @ (B, H_Q, Head_Dim, Seq_Len_KV) -> (B, H_Q, 1, Seq_Len_KV)
+        scores = torch.matmul(xq,keys.transpose(2,3)) / math.sqrt(self.head_dim)
+        scores = F.softmax(scores.float(),dim=-1).type_as(xq)
+        
+        # (B, H_Q, 1, Seq_Len_KV) @ (B, H_Q, Seq_Len_KV, Head_Dim) -> (B, H_Q, 1, Head_Dim)
+        output = torch.matmul(scores,values)
+
+        # (B, H_Q, 1, Head_Dim) -> (B, 1, H_Q, Head_Dim) -> (B, 1, Dim)
+        output = output.transpose(1,2).contiguous().view(batch_size,seq_len,-1)
+
+        return self.wo(output) # (B, 1, Dim) -> (B, 1, Dim)
+
 
 class DecoderBlock(nn.Module):
 
@@ -111,11 +209,11 @@ class DecoderBlock(nn.Module):
         # Calculating RMSNorm BEFORE Attention
         self.attention_norm = RMSNorm(args.dim, args.norm_eps)
         # Calculating RMSNorm AFTER Feedforward layer
-        self.ffn_norm = RMSNorm(args.dim,args.norm_eps)
+        self.ffn_norm = RMSNorm(args.dim, args.norm_eps)
 
-    def forward(self,x: torch.Tensor, start_pos:int, freq_complex: torch.Tensor):
+    def forward(self, x: torch.Tensor, start_pos: int, freq_complex: torch.Tensor):
         # (B, Seq_Len, Dim) + (B, Seq_Len, Dim) = (B, Seq_Len, Dim)
-        h = x + self.attention(self.attention_norm(x),start_pos,freq_complex)
+        h = x + self.attention(self.attention_norm(x), start_pos, freq_complex)
         out = self.feed_forward(self.ffn_norm(h))
         return out
 
@@ -153,12 +251,12 @@ class Transformer:
         h = self.tok_embeddings(tokens)
 
         # Retrieve pairs of (m,theta) uptill the positions [start_pos,start_pos+seq_len]
-        freq_complex = self.freqs_complex[start_pos:start_pos+seq_len]
+        freq_complex = self.freqs_complex[start_pos : start_pos + seq_len]
 
         # Consecutively apply to all decoder layers
         for layer in self.layers:
-            h = layer(h,start_pos,freq_complex)
-        
+            h = layer(h, start_pos, freq_complex)
+
         h = self.norm(h)
         output = self.output(h)
         return output
